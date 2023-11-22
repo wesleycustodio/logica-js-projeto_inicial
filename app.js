d1516946675f0513654a0fd13510ab58fb89e534
wesley <wesley.sergio.custodio@gmail.com>
2023-11-21 22:01:28 -0300

    alteração js

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 alert('Boas vindas ao jogo do número secreto');
-let numeroSecreto = 5;
+let numeroSecreto = 9;
+console.log(numeroSecreto);
 let chute = prompt('Escolha um número entre 1 e 10'); 
 
 
+
 if (chute == numeroSecreto){
-    console.log('Isso ai! Você descobriu o número secreto (5)');
+    alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto}`);  
+} else {
+    alert('Você Errou');
 }

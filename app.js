9d109a312d7a8eead5b55ff48e71e7235aa5b0a7
wesley <wesley.sergio.custodio@gmail.com>
2023-11-25 09:18:05 -0300

    app js

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ let numeroSecreto = 9;
 console.log(numeroSecreto);
 let chute = prompt('Escolha um número entre 1 e 10'); 
 
-if (chute == numeroSecreto){
-    alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto}`);  
+if (chute == numeroSecreto) {
+    alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto}`);
 } else {
-    if (chute > numeroSecreto);
-        alert(`o numero secreto é menor que ${chute}`);
-    } else{
-        alert(`O numero secreto é maior que ${chute}`);
+    if (chute > numeroSecreto) {
+            alert(`O número secreto é menor que ${chute}`);
+    } else {
+            alert(`O número secreto é maior que ${chute}`);
+    }
 }
-

79261f5fea938d8ea31cd1fd3c56b4f760541eea
Wesley Custodio <wesley.custodio@vegus.com.br>
2023-12-04 14:56:56 -0300

    adição no app js

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 alert('Boas vindas ao jogo do número secreto');
-let numeroSecreto = 5;
+let numeroSecreto = 2;
+console.log(numeroSecreto);
 let chute = prompt('Escolha um número entre 1 e 10'); 
 
-
-if (chute == numeroSecreto){
-    console.log('Isso ai! Você descobriu o número secreto (5)');
+//se chute for igual ao numero secreto
+if (chute == numeroSecreto) {
+    alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto}`);
+} else {
+    if (chute > numeroSecreto){
+        alert('O numero secreto é maior que ')
+    }
 }
